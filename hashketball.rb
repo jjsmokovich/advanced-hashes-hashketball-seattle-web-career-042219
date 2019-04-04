0eeb4fc88f1c5da38c58cc32457ab81a74cd67d6
Jesse Smokovich <jessesmokovich@yahoo.com>
2019-04-04 04:26:43 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -149,7 +149,7 @@ def team_names
 end
 
 def team_list(team_name)
-  game_hash.values.find {|team|[:team_name] == team_name}
+  game_hash.values.find {|team| team[:team_name] == team_name}
 end
 
 def player_numbers(team_name)
